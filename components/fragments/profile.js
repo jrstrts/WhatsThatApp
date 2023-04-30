8455e51025ecbb9498b94f543fa0c0738464d051
Joshua Struthers <struthers64@gmail.com>
2023-04-30 12:01:58 +0100

    Add functionality to Logout button
    Added functionality to the logout button

--- components/fragments/profile.js
+++ components/fragments/profile.js
@@ -1,14 +1,36 @@
 import {View, Pressable, Text, StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 class Profile extends Component {
   static propTypes = {
     navigation: PropTypes.any,
   };
 
-  logout = () => {
+  logout = async () => {
     console.log('log out');
+
+    return fetch('http://localhost:3333/api/1.0.0/logout', {
+      method: 'post',
+      headers: {
+        'X-Authorization': await AsyncStorage.getItem('session_token'),
+      },
+    })
+        .then(async (response) => {
+          if (response.status === 200) {
+            await AsyncStorage.removeItem('session_token');
+            await AsyncStorage.removeItem('user_id');
+            this.props.navigation.navigate('LoginNav');
+          } else if (response.status === 401) {
+            console.log('Not logged in!');
+            this.props.navigation.navigate('LoginNav');
+          } else if (response.status === 500) {
+            console.log('Server error!');
+          } else {
+            console.log('Something went wrong!');
+          }
+        });
   };
 
   render() {
