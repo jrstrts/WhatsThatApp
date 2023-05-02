8fa37e716fc373b33356ea79c445a65a665eab65
Joshua Struthers <struthers64@gmail.com>
2023-05-02 23:24:14 +0100

    Implement contacts endpoint
    implement the contacts endpoint, showing a users contacts

--- components/fragments/contacts.js
+++ components/fragments/contacts.js
@@ -1,23 +1,64 @@
-/* eslint-disable max-len */
-
-import {ScrollView} from 'react-native';
+import {ScrollView, FlatList, View, Text} from 'react-native';
 import React, {Component} from 'react';
 import User from '../elements/user';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 class Contacts extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      isProcessing: false,
+      firstName: '',
+      lastName: '',
+      contactData: [],
+    };
+  }
+
+  getContacts = async () => {
+    return fetch('http://localhost:3333/api/1.0.0/contacts', {
+      headers: {
+        'X-Authorization': await AsyncStorage.getItem('session_token'),
+      },
+    })
+        .then((response) => response.json())
+        .then((responseJson) => {
+          this.setState({
+            isLoading: false,
+            contactData: responseJson,
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+  };
+
+  componentDidMount() {
+    this.getContacts();
+  }
+
   render() {
-    return (
-      <ScrollView>
-        <User contactName='Contact 1' />
-        <User contactName='Contact 2' />
-        <User contactName='Contact 3' />
-        <User contactName='Contact 4' />
-        <User contactName='Contact 5' />
-        <User contactName='Contact 6' />
-        <User contactName='Contact 7' />
-        <User contactName='Contact 8' />
-      </ScrollView>
-    );
+    if (this.state.isLoading) {
+      return (
+        <View>
+          <Text>Loading...</Text>
+        </View>
+      );
+    } else {
+      return (
+        <ScrollView>
+          <FlatList
+            data={this.state.contactData}
+            renderItem={({item}) => (
+              <User
+                contactName={`${item.first_name} ${item.last_name}`}
+              />
+            )}
+          />
+        </ScrollView>
+      );
+    }
   }
 }
 
