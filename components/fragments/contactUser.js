b370aeb0190952172ee4e058fdf35cb18e2bcbd8
Joshua Struthers <struthers64@gmail.com>
2023-05-03 19:27:32 +0100

    Allow users to add and remove contacts
    When entering a user, determine whether they are already a contact, then show appropriate button.
    
    implement an add contact button.
    
    I think I'm using if statements in the render function wrong, but it works :sweat_smile:

--- components/fragments/contactUser.js
+++ components/fragments/contactUser.js
@@ -4,6 +4,16 @@ import PropTypes from 'prop-types';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 class ContactUser extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      contactData: [],
+      isContact: false,
+    };
+  };
+
   static propTypes = {
     route: PropTypes.any,
     navigation: PropTypes.any,
@@ -32,41 +42,145 @@ class ContactUser extends Component {
         });
   };
 
-  render() {
-    return (
-      <View>
-        <View style={userStyles.txtContainer}>
-          <Text style={userStyles.titleText}>Contact Details</Text>
-        </View>
-        <View style={userStyles.txtContainer}>
-          <Text style={userStyles.subHeadText}>First Name</Text>
-          <Text style={userStyles.elements}>
-            {this.props.route.params.firstName}
-          </Text>
-          <Text style={userStyles.subHeadText}>Last Name</Text>
-          <Text style={userStyles.elements}>
-            {this.props.route.params.lastName}
-          </Text>
-          <Text style={userStyles.subHeadText}>Email</Text>
-          <Text style={userStyles.elements}>
-            {this.props.route.params.email}
-          </Text>
-        </View>
+  addContact = async () => {
+    return fetch(`http://localhost:3333/api/1.0.0/user/${this.props.route.params.userID}/contact`, {
+      method: 'POST',
+      headers: {
+        'X-Authorization': await AsyncStorage.getItem('session_token'),
+      },
+    })
+        .then((response) => {
+          if (response.status === 200) {
+            console.log('200: OK');
+            this.props.navigation.goBack();
+          } else if (response.status === 400) {
+            console.log('400: Removing yourself');
+          } else if (response.status === 401) {
+            console.log('401: Not Logged in');
+          } else if (response.status === 404) {
+            console.log('404: User not found');
+          } else {
+            console.log('Something went wrong!');
+          }
+        });
+  };
 
-        <View style={userStyles.txtContainer}>
-          <Text style={userStyles.titleText}>Remove Contact</Text>
+  isContact = async () => {
+    this.setState({isLoading: true});
+    return fetch('http://localhost:3333/api/1.0.0/contacts', {
+      headers: {
+        'X-Authorization': await AsyncStorage.getItem('session_token'),
+      },
+    })
+        .then((response) => response.json())
+        .then((responseJson) => {
+          this.setState({
+            contactData: responseJson,
+          }, () => {
+            try {
+              this.state.contactData.find(
+                  (x) => x.user_id === this.props.route.params.userID,
+              ).user_id;
+              console.log('user is a contact');
+              this.setState({isContact: true, isLoading: false});
+            } catch (TypeError) {
+              console.log('User is not a contact');
+              this.setState({isContact: false, isLoading: false});
+            }
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+  };
+
+  componentDidMount() {
+    this.props.navigation.addListener('focus', this.onFocus);
+  }
+
+  onFocus = () => {
+    this.isContact();
+    console.log(this.state.isContact);
+  };
+
+  render() {
+    if (this.state.isLoading === true) {
+      return (
+        <View>
+          <Text>Loading...</Text>
         </View>
-        <View style={userStyles.buttonContainer}>
-          <Pressable
-            style={[userStyles.removeButton, userStyles.elements]}
-            onPress={() => {
-              this.removeContact();
-            }}>
-            <Text style={userStyles.buttonText}>Remove</Text>
-          </Pressable>
+      );
+    } else if (this.state.isContact === true) {
+      return (
+        <View>
+          <View style={userStyles.txtContainer}>
+            <Text style={userStyles.titleText}>Contact Details</Text>
+          </View>
+          <View style={userStyles.txtContainer}>
+            <Text style={userStyles.subHeadText}>First Name</Text>
+            <Text style={userStyles.elements}>
+              {this.props.route.params.firstName}
+            </Text>
+            <Text style={userStyles.subHeadText}>Last Name</Text>
+            <Text style={userStyles.elements}>
+              {this.props.route.params.lastName}
+            </Text>
+            <Text style={userStyles.subHeadText}>Email</Text>
+            <Text style={userStyles.elements}>
+              {this.props.route.params.email}
+            </Text>
+          </View>
+
+          <View style={userStyles.txtContainer}>
+            <Text style={userStyles.titleText}>Remove Contact</Text>
+          </View>
+          <View style={userStyles.buttonContainer}>
+            <Pressable
+              style={[userStyles.removeButton, userStyles.elements]}
+              onPress={() => {
+                this.removeContact();
+              }}>
+              <Text style={userStyles.buttonText}>Remove</Text>
+            </Pressable>
+          </View>
         </View>
-      </View>
-    );
+      );
+    } else if (this.state.isContact === false) {
+      return (
+        <View>
+          <View style={userStyles.txtContainer}>
+            <Text style={userStyles.titleText}>Contact Details</Text>
+          </View>
+          <View style={userStyles.txtContainer}>
+            <Text style={userStyles.subHeadText}>First Name</Text>
+            <Text style={userStyles.elements}>
+              {this.props.route.params.firstName}
+            </Text>
+            <Text style={userStyles.subHeadText}>Last Name</Text>
+            <Text style={userStyles.elements}>
+              {this.props.route.params.lastName}
+            </Text>
+            <Text style={userStyles.subHeadText}>Email</Text>
+            <Text style={userStyles.elements}>
+              {this.props.route.params.email}
+            </Text>
+          </View>
+
+          <View style={userStyles.txtContainer}>
+            <Text style={userStyles.titleText}>Add Contact</Text>
+          </View>
+          <View style={userStyles.buttonContainer}>
+            <Pressable
+              style={[userStyles.addButton, userStyles.elements]}
+              onPress={() => {
+                this.addContact();
+              }}>
+              <Text style={userStyles.buttonText}>Add</Text>
+            </Pressable>
+          </View>
+        </View>
+      );
+    }
   }
 }
 
@@ -90,6 +204,14 @@ const userStyles = StyleSheet.create({
   },
   removeButton: {
     backgroundColor: 'red',
+    borderRadius: 10,
+    width: '30%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  addButton: {
+    backgroundColor: '#54ADFF',
     borderRadius: 10,
     width: '30%',
     height: 50,
