edc2da471efada037a4ff2a414c2606c8715e148
Joshua Struthers <struthers64@gmail.com>
2023-05-03 16:43:36 +0100

    Add functionality to search page
    Add search box, users flatlist, get request for search, and pagination support

--- components/fragments/search.js
+++ components/fragments/search.js
@@ -1,14 +1,217 @@
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Pressable,
+  FlatList,
+} from 'react-native';
 import React, {Component} from 'react';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import User from '../elements/user';
 
 class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchEntry: '',
+      isLoading: false,
+      hasSearched: false,
+      searchData: [],
+      pageNo: 1,
+    };
+  }
+
+  handleSearchEntryInput = (searchEntry) => {
+    this.setState({searchEntry: searchEntry});
+  };
+
+  getSearchResults = async () => {
+    this.setState({
+      isLoading: true,
+      searchData: [],
+    });
+
+    return fetch('http://localhost:3333/api/1.0.0/search/?q=' +
+                  this.state.searchEntry +
+                  '&?search_in=all&limit=5&offset=' +
+                  (this.state.pageNo-1)*5, {
+      headers: {
+        'X-Authorization': await AsyncStorage.getItem('session_token'),
+      },
+    })
+        .then((response) => response.json())
+        .then((responseJson) => {
+          this.setState({
+            isLoading: false,
+            hasSearched: true,
+            searchData: responseJson,
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+  };
+
+  handlePageIncrement() {
+    if (this.state.pageNo >= 1) {
+      this.setState({pageNo: this.state.pageNo + 1}, () => {
+        this.getSearchResults();
+      });
+    }
+  };
+
+  handlePageDecrement() {
+    if (this.state.pageNo > 1) {
+      this.setState({pageNo: this.state.pageNo - 1}, () => {
+        this.getSearchResults();
+      });
+    }
+  };
+
   render() {
-    return (
-      <View>
-        <Text>Test Page!</Text>
-      </View>
-    );
+    if (!this.state.hasSearched || this.state.isLoading) {
+      return (
+        <View>
+          <View style={SearchStyles.txtContainer}>
+            <Text style={SearchStyles.titleText}>Search</Text>
+            <Text>Search for a user to add to your contacts</Text>
+          </View>
+          <View style={SearchStyles.inputContainer}>
+            <TextInput
+              style={[SearchStyles.inputs, SearchStyles.elements]}
+              placeholderTextColor={'grey'}
+              inputMode='text'
+              placeholder='Search'
+              onChangeText={this.handleSearchEntryInput}
+              value={this.state.searchEntry}
+            />
+            <Pressable
+              style={SearchStyles.searchButton}
+              onPress={() => this.getSearchResults()}
+            >
+              <Ionicons name='search-outline' size={32} />
+            </Pressable>
+          </View>
+
+          <View style={SearchStyles.separator}/>
+        </View>
+      );
+    } else {
+      return (
+        <View>
+          <View style={SearchStyles.txtContainer}>
+            <Text style={SearchStyles.titleText}>Search</Text>
+            <Text>Search for a user to add to your contacts</Text>
+          </View>
+          <View style={SearchStyles.inputContainer}>
+            <TextInput
+              style={[SearchStyles.inputs, SearchStyles.elements]}
+              placeholderTextColor={'grey'}
+              inputMode='text'
+              placeholder='Search'
+              onChangeText={this.handleSearchEntryInput}
+              value={this.state.searchEntry}
+            />
+            <Pressable
+              style={SearchStyles.searchButton}
+              onPress={() => this.getSearchResults()}
+            >
+              <Ionicons name='search-outline' size={32} />
+            </Pressable>
+          </View>
+
+          <View style={SearchStyles.separator}/>
+
+          <FlatList
+            data={this.state.searchData}
+            renderItem={({item}) => (
+              <Pressable onPress={() => console.log('Contact Pressed!')}>
+                <User
+                  contactName={`${item.given_name} ${item.family_name}`}
+                />
+              </Pressable>
+            )}
+          />
+
+          <View style={SearchStyles.pageButtonContainer}>
+            <Pressable
+              style={SearchStyles.navArrowButtons}
+              onPress={() => this.handlePageDecrement()}>
+              <Ionicons name='caret-back' size={32} />
+            </Pressable>
+            <Text>{this.state.pageNo}</Text>
+            <Pressable
+              style={SearchStyles.navArrowButtons}
+              onPress={() =>this.handlePageIncrement()}>
+              <Ionicons name='caret-forward' size={32} />
+            </Pressable>
+          </View>
+        </View>
+      );
+    }
   }
 }
+
+const SearchStyles = StyleSheet.create({
+  txtContainer: {
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    paddingLeft: '10%',
+    padding: 15,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingLeft: '10%',
+  },
+  pageButtonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  separator: {
+    padding: 20,
+  },
+  inputs: {
+    borderWidth: 1,
+    borderRadius: 10,
+    width: '70%',
+    height: 50,
+    backgroundColor: 'lightgrey',
+    borderColor: '#fff',
+    marginRight: 10,
+  },
+  elements: {
+    padding: 5,
+    marginTop: 5,
+    marginBottom: 5,
+  },
+  searchButton: {
+    backgroundColor: '#BABABA',
+    borderRadius: 10,
+    width: '20%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  navArrowButtons: {
+    width: '20%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+  },
+  titleText: {
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+});
 
 export default Search;
