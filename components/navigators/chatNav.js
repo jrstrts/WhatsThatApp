d165a3d4181286ae779833a98f10db8800ee7b24
Joshua Struthers <struthers64@gmail.com>
2023-05-06 17:26:29 +0100

    Add info button to chats

--- components/navigators/chatNav.js
+++ components/navigators/chatNav.js
@@ -1,6 +1,9 @@
 // import {StyleSheet} from 'react-native';
 import React, {Component} from 'react';
+import {Pressable} from 'react-native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import PropTypes from 'prop-types';
 
 import Chats from '../fragments/chat';
 import MessageThread from '../fragments/message-thread';
@@ -9,6 +12,10 @@ import AddChat from '../fragments/addChat';
 const Stack = createNativeStackNavigator();
 
 class ChatNav extends Component {
+  static propTypes = {
+    navigation: PropTypes.any,
+  };
+
   render() {
     return (
       <Stack.Navigator screenOptions={{headerShown: false}} >
@@ -16,7 +23,17 @@ class ChatNav extends Component {
         <Stack.Screen
           name="ChatScreen"
           component={MessageThread}
-          options={{headerShown: true}} />
+          options={{headerShown: true,
+            headerRight: () => (
+              <Pressable
+                onPress={console.log('info')}
+                style={{paddingRight: 20}}
+              >
+                <Ionicons name='information-circle-outline' size={32} />
+              </Pressable>
+            ),
+          }}
+        />
         <Stack.Screen
           name='AddChat'
           component={AddChat}
