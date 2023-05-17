61d3e5512a582d25731bb3748c800659802a7ee1
Joshua Struthers <struthers64@gmail.com>
2023-05-17 19:35:14 +0100

    Create global style sheet for modals

---
+++ components/styles/globalStyles.js
@@ -0,0 +1,25 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 22,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+    borderColor: 'rgba(0, 0, 0, 0.1)',
+  },
+  bottomModal: {
+    justifyContent: 'flex-end',
+    margin: 0,
+  },
+  modalButton: {
+    backgroundColor: 'lightblue',
+    padding: 12,
+    margin: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+    borderColor: 'rgba(0, 0, 0, 0.1)',
+  },
+});
