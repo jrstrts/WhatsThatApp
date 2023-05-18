557ca1b895fb7e1232ce41b7a59c4bbd4f7d290f
Joshua Struthers <struthers64@gmail.com>
2023-05-18 19:07:56 +0100

    Fix validation and add modals to changeDetails

--- components/fragments/changeDetails.js
+++ components/fragments/changeDetails.js
@@ -2,6 +2,11 @@ import {View, Pressable, Text, StyleSheet, TextInput} from 'react-native';
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Modal from 'react-native-modal';
+import globalStyles from '../styles/globalStyles';
+import PasswordValidator from 'password-validator';
+
+const validator = require('email-validator');
 
 class ChangeDetails extends Component {
   constructor(props) {
@@ -14,6 +19,8 @@ class ChangeDetails extends Component {
       password: '',
       statusText: '',
       statusColor: 'black',
+      visibleModal: null,
+      errorMessage: '',
     };
   }
 
@@ -60,56 +67,92 @@ class ChangeDetails extends Component {
         .then(async (responseJson) => {
           // const is innapropriate for next line, but eslint doesn't agree
           // eslint-disable-next-line prefer-const
-          let dataToSend = {};
+          const pwSchema = new PasswordValidator();
+          pwSchema
+              .is().min(8)
+              .has().uppercase()
+              .has().lowercase()
+              .has().digits(1)
+              .has().symbols(1);
 
-          if (this.state.firstName !== responseJson['first_name'] &&
-              this.state.firstName !== '') {
-            dataToSend['first_name'] = this.state.firstName;
-          }
+          if ((validator.validate(this.state.email) || this.state.email == '') &&
+          (pwSchema.validate(this.state.password) || this.state.password == '')) {
+            let dataToSend = {};
 
-          if (this.state.lastName !== responseJson['last_name'] &&
-              this.state.lastName !== '') {
-            dataToSend['last_name'] = this.state.lastName;
-          }
+            if (this.state.firstName !== responseJson['first_name'] &&
+                this.state.firstName !== '') {
+              dataToSend['first_name'] = this.state.firstName;
+            }
 
-          if (this.state.email !== responseJson['email'] &&
-              this.state.email !== '') {
-            dataToSend['email'] = this.state.email;
-          }
+            if (this.state.lastName !== responseJson['last_name'] &&
+                this.state.lastName !== '') {
+              dataToSend['last_name'] = this.state.lastName;
+            }
 
-          if (this.state.password !== '') {
-            dataToSend['password'] = this.state.password;
-          }
+            if (this.state.email !== responseJson['email'] &&
+                this.state.email !== '') {
+              dataToSend['email'] = this.state.email;
+            }
 
-          dataToSend = JSON.stringify(dataToSend);
-          if (dataToSend !== '{}') {
-            return fetch('http://localhost:3333/api/1.0.0/user/' +
-                          await AsyncStorage.getItem('user_id'), {
-              method: 'PATCH',
-              headers: {
-                'X-Authorization': await AsyncStorage.getItem('session_token'),
-                'content-type': 'application/json',
-              },
-              body: dataToSend,
-            })
-                .then((response) => {
-                  if (response.status === 200) {
-                    console.log('Update success!');
-                    this.props.navigation.navigate('Profile');
-                  } else if (response.status === 400) {
-                    console.log('400: Bad Request (bad data)');
-                  } else if (response.status === 403) {
-                    console.log('403: Forbidden (editing wrong user?)');
-                  } else if (response.status === 404) {
-                    console.log('404: Not Found (user does not exist)');
-                  } else if (response.status === 401 ||
-                             response.status === 500) {
-                    console.log('401 or 500: Something has gone wrong!');
-                  }
-                })
-                .catch((error) => {
-                  console.log(error);
-                });
+            if (this.state.password !== '') {
+              dataToSend['password'] = this.state.password;
+            }
+
+            dataToSend = JSON.stringify(dataToSend);
+            if (dataToSend !== '{}') {
+              return fetch('http://localhost:3333/api/1.0.0/user/' +
+                            await AsyncStorage.getItem('user_id'), {
+                method: 'PATCH',
+                headers: {
+                  'X-Authorization':
+                    await AsyncStorage.getItem('session_token'),
+                  'content-type': 'application/json',
+                },
+                body: dataToSend,
+              })
+                  .then((response) => {
+                    if (response.status === 200) {
+                      console.log('Update success!');
+                      this.props.navigation.navigate('Profile');
+                    } else if (response.status === 400) {
+                      console.log('Invalid email or password (server side)');
+                      this.setState({
+                        visibleModal: 1,
+                        errorMessage: 'Your email is invalid, or password is not strong enough. Please try again',
+                      });
+                    } else if (response.status === 404) {
+                      console.log('404 Error');
+                      this.setState({
+                        visibleModal: 1,
+                        errorMessage: 'Please log out and try again',
+                      });
+                    } else {
+                      console.log('Other error');
+                      this.setState({
+                        visibleModal: 1,
+                        errorMessage: 'There was a problem processing your request. Please try again',
+                      });
+                    }
+                  })
+                  .catch((error) => {
+                    console.log(error);
+                    this.setState({
+                      visibleModal: 1,
+                      errorMessage: 'There was a problem processing your request. Please try again',
+                    });
+                  });
+            } else {
+              this.setState({
+                visibleModal: 1,
+                errorMessage: 'No changes detected.',
+              });
+            }
+          } else {
+            console.log('Invalid email/password (client side)');
+            this.setState({
+              visibleModal: 1,
+              errorMessage: 'Your email is invalid, or password is not strong enough. Please try again',
+            });
           }
         });
   };
@@ -166,6 +209,17 @@ class ChangeDetails extends Component {
             <Text style={ChangeDetailsStyles.buttonText}>Submit</Text>
           </Pressable>
         </View>
+        <Modal isVisible={this.state.visibleModal === 1}
+          style={globalStyles.bottomModal}>
+          <View style={globalStyles.modalContent}>
+            <Text>{this.state.errorMessage}</Text>
+            <Pressable onPress={() => this.setState({visibleModal: null})}>
+              <View style={globalStyles.modalButton}>
+                <Text>Close</Text>
+              </View>
+            </Pressable>
+          </View>
+        </Modal>
       </View>
     );
   }
